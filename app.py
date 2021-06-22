e4dab4f723b16294ac9820a30a3aadcbaaa753d3
Anízio Campos Pereira <aniziocp@poli.ufrj.br>
2021-06-22 18:30:28 -0300

    Withdraw pycaret import
    Retirei o import do pycaret

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import streamlit as st
-from pycaret.regression import *
+#from pycaret.regression import *
 
 # loading the trained model.
 model = load_model('model/modelo-final')
