ec20acb2d8a01656cb471bdee8a64ee5a85e9aa8
Anizio <aniziocp@poli.ufrj.br>
2021-06-23 22:00:30 -0300

    deploying
    deploying

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import streamlit as st
-from sklearn.externals import joblib
-#from pycaret.regression import *
+from pycaret.regression import *
+#import pickle
 
 # loading the trained model.
-#model = load_model('model/modelo-final')
-model = joblib.load('model/modelo-final')
+model = load_model('model/modelo-final')
+#model = pickle.load('model/modelo-final')
 
 
 # carregando uma amostra dos dados.
@@ -25,11 +25,11 @@ st.sidebar.subheader("Defina os atributos do imóvel para predição do aluguel"
 
 
 # mapeando dados do usuário para cada atributo
-area = st.sidebar.number_input("Área total", value=None)
-num_quartos = st.sidebar.number_input("Número de Quartos", value=None)
-num_banheiros = st.sidebar.number_input("Número de Banheiros", value=None)
-vagas_garagem = st.sidebar.number_input("Vagas de Garagem", value=None)
-num_andares = st.sidebar.number_input("Número de Andares", value=None)
+area = st.sidebar.number_input("Área total", value=0)
+num_quartos = st.sidebar.number_input("Número de Quartos", value=0)
+num_banheiros = st.sidebar.number_input("Número de Banheiros", value=0)
+vagas_garagem = st.sidebar.number_input("Vagas de Garagem", value=0)
+num_andares = st.sidebar.number_input("Número de Andares", value=0)
 aceita_animais = st.sidebar.selectbox("Aceita Animais?",("Sim","Não"))
 mobilia = st.sidebar.selectbox("Mobiliado?",("Sim","Não"))
 
@@ -38,13 +38,13 @@ aceita_animais = 1 if aceita_animais == "Sim" else 0
 mobilia = 1 if mobilia == "Sim" else 0
 
 
-estado = st.sidebar.selectbox("Estado",("SP", "RJ", "MG", "SP", "RS"))
+estado = st.sidebar.selectbox("Estado",("SP", "RJ", "MG", "RS"))
 
 cidade = st.sidebar.selectbox("Cidade", ("São Paulo", "Rio de Janeiro", "Belo Horizonte", "Campinas", "Porto Alegre"))
 
-valor_condominio = st.sidebar.number_input("Valor do Condomínio", value=dataset["valor_condominio"].mean())
-valor_iptu = st.sidebar.number_input("Valor do IPTU", value=dataset["valor_iptu"].mean())
-valor_seguro_incendio = st.sidebar.number_input("Valor do Seguro Incêndio", value=dataset["valor_seguro_incendio"].mean())
+valor_condominio = st.sidebar.number_input("Valor do Condomínio", value=0)
+valor_iptu = st.sidebar.number_input("Valor do IPTU", value=0)
+valor_seguro_incendio = st.sidebar.number_input("Valor do Seguro Incêndio", value=0)
 
 # inserindo um botão na tela
 btn_predict = st.sidebar.button("Realizar Predição")
